b7a0ec34fe8769819be7dae29b32c1900cc56a45
Zablon-max <zablonisadia8713@gmail.com>
2026-06-19 10:00:00 +0300

    Change file paths to uppercase in service worker
    Updated file paths to use uppercase 'Bless' instead of lowercase 'bless'.

--- Service worker.js
+++ Service worker.js
@@ -1,8 +1,8 @@
 var CACHE = 'blessings-v1';
 var FILES = [
-  '/bless/index.html',
-  '/bless/index.css',
-  '/bless/manifest.json'
+  '/Bless/index.html',
+  '/Bless/index.css',
+  '/Bless/manifest.json'
 ];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
